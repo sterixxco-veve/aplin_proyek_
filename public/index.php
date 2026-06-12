27df135e5cdfd28e925378c50b10541ed48c2dba
sterixxco-veve <anastasiaevelyn11@gmail.com>
2026-06-12 22:54:24 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -8,17 +8,7 @@ define('LARAVEL_START', microtime(true));
 require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
-echo '<pre>';
-echo 'view bound = ';
-var_dump($app->bound('view'));
 
-echo 'router bound = ';
-var_dump($app->bound('router'));
-
-echo 'config bound = ';
-var_dump($app->bound('config'));
-
-exit;
 $kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
