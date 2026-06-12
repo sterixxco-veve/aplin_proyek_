44dc3ff784154bf26abd06ff3fe02188c07d7b10
sterixxco-veve <anastasiaevelyn11@gmail.com>
2026-06-12 21:11:30 +0700

    aa

--- api/index.php
+++ api/index.php
@@ -1,4 +1,9 @@
 <?php
 
-// Mengarahkan Vercel serverless agar membaca public/index.php bawaan Laravel
+// 1. Jalankan migrasi otomatis secara aman di cloud Vercel
+$app = require __DIR__ . '/../bootstrap/app.php';
+$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+$kernel->call('migrate', ['--force' => true]);
+
+// 2. Lanjutkan render halaman Laravel seperti biasa
 require __DIR__ . '/../public/index.php';
