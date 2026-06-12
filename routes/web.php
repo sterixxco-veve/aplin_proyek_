a49e195323fdd8239117d687c4827dff420be5ff
sterixxco-veve <anastasiaevelyn11@gmail.com>
2026-06-12 21:43:54 +0700

    tesss

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json([
-        'status' => 'ok',
-        'php' => phpversion(),
-    ]);
+    return 'Laravel OK';
 });
