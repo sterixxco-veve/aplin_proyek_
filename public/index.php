f9467c5f8ff762c03370f0966908eeb1209aaec3
sterixxco-veve <anastasiaevelyn11@gmail.com>
2026-06-12 22:45:37 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,82 +1,30 @@
 <?php
-echo "STEP 1";
-require __DIR__.'/../vendor/autoload.php';
 
-echo "STEP 2";
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-echo "STEP 3";
-$kernel = $app->make(Kernel::class);
-
-echo "STEP 4";
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
-
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
-
 require __DIR__.'/../vendor/autoload.php';
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
-
 $app = require_once __DIR__.'/../bootstrap/app.php';
+echo '<pre>';
+echo 'view bound = ';
+var_dump($app->bound('view'));
 
-try {
+echo 'router bound = ';
+var_dump($app->bound('router'));
 
-    $kernel = $app->make(Kernel::class);
+echo 'config bound = ';
+var_dump($app->bound('config'));
 
-    $response = $kernel->handle(
-        $request = Request::capture()
-    );
+exit;
+$kernel = $app->make(Kernel::class);
 
-    $response->send();
+$response = $kernel->handle(
+    $request = Request::capture()
+);
 
-    $kernel->terminate($request, $response);
+$response->send();
 
-} catch (\Throwable $e) {
-
-    echo '<pre>';
-    echo get_class($e);
-    echo "\n\n";
-    echo $e->getMessage();
-    echo "\n\n";
-    echo $e->getFile();
-    echo ':'.$e->getLine();
-    echo "\n\n";
-    echo $e->getTraceAsString();
-    exit;
-}
+$kernel->terminate($request, $response);
