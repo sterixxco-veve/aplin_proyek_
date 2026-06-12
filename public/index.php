f3ad59ad6bd73a8fb9b75dd903e365cf05408893
sterixxco-veve <anastasiaevelyn11@gmail.com>
2026-06-12 23:22:41 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,22 +1,20 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
 
-return Application::configure(
-    basePath: dirname(__DIR__)
-)
-->withRouting(
-    web: __DIR__.'/../routes/web.php',
-    api: __DIR__.'/../routes/api.php',
-    commands: __DIR__.'/../routes/console.php',
-    health: '/up',
-)
-->withMiddleware(function (Middleware $middleware) {
-    //
-})
-->withExceptions(function (Exceptions $exceptions) {
-    //
-})
-->create();
+define('LARAVEL_START', microtime(true));
+
+require __DIR__.'/../vendor/autoload.php';
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
