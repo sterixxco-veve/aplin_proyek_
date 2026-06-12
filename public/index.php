a7641203357e0e060cc185c5dd9e8bbd67d4abf6
sterixxco-veve <anastasiaevelyn11@gmail.com>
2026-06-12 23:08:18 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -6,7 +6,9 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 require __DIR__.'/../vendor/autoload.php';
-
+echo "<pre>";
+echo "APP LOADED";
+exit;
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
