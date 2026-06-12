54a4851f5331a331d0cc455d21698aa5a7222604
sterixxco-veve <anastasiaevelyn11@gmail.com>
2026-06-12 23:31:13 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -7,14 +7,22 @@ define('LARAVEL_START', microtime(true));
 
 require __DIR__.'/../vendor/autoload.php';
 
+echo "STEP 1<br>";
+
 $app = require_once __DIR__.'/../bootstrap/app.php';
+
+echo "STEP 2<br>";
 
 $kernel = $app->make(Kernel::class);
 
-$response = $kernel->handle(
-    $request = Request::capture()
-);
+echo "STEP 3<br>";
+
+$request = Request::capture();
+
+echo "STEP 4<br>";
+
+$response = $kernel->handle($request);
+
+echo "STEP 5<br>";
 
 $response->send();
-
-$kernel->terminate($request, $response);
