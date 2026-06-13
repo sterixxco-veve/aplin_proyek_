145eb11501195bf0404cf0b45c18cf074e20498a
sterixxco-veve <anastasiaevelyn11@gmail.com>
2026-06-13 12:09:15 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(
+$app = Application::configure(
     basePath: dirname(__DIR__)
 )
 ->withRouting(
@@ -14,9 +14,11 @@ return Application::configure(
     health: '/up',
 )
 ->withMiddleware(function (Middleware $middleware) {
-    //
 })
 ->withExceptions(function (Exceptions $exceptions) {
-    //
 })
 ->create();
+
+echo "BOOTSTRAP OK\n";
+
+return $app;
