fb6147f09cb337e8ba222826984b7828c6e082ed
sterixxco-veve <anastasiaevelyn11@gmail.com>
2026-07-09 08:08:59 +0700

    Buat Update Host

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,11 +2,12 @@
 
 @section('content')
 
-<div class="text-center mt-5">
-    <h1>Welcome to Event Manager 🚀</h1>
 
-    <a href="/login" class="btn btn-dark mt-3">Login</a>
-    <a href="/register" class="btn btn-primary mt-3">Register</a>
-</div>
+    <div class="text-center mt-5">
+        <h1>Welcome to Event Manager 🚀</h1>
+
+        <a href="/login" class="btn btn-dark mt-3">Login</a>
+        <a href="/register" class="btn btn-primary mt-3">Register</a>
+    </div>
 
 @endsection
